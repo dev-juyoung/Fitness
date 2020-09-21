cef761d4a3df8d2f5dd33de7e06285095bb2ac1f
Juyoung, Lee <dev.juyoung@gmail.com>
2020-09-21 09:23:15 +0900

    [ADDED] Implemented the TimeRange class to reduce errors in the time range on read requests.

--- lib/src/fitness.dart
+++ lib/src/fitness.dart
@@ -26,20 +26,20 @@ class Fitness {
   }
 
   static Future<List<DataPoint>> read({
-    int dateFrom,
-    int dateTo,
-    int bucketByTime,
-    TimeUnit timeUnit,
+    TimeRange timeRange,
+    int bucketByTime = 1,
+    TimeUnit timeUnit = TimeUnit.days,
   }) async {
-    return _channel.invokeListMethod(
-      'read',
-      {
-        'date_from': dateFrom ?? DateTime.now().min,
-        'date_to': dateTo ?? DateTime.now().max,
-        'bucket_by_time': bucketByTime,
-        'time_unit': timeUnit.value,
-      },
-    ).then(
+    return _channel.invokeListMethod('read', {
+      'date_from': timeRange != null
+          ? timeRange.start.millisecondsSinceEpoch
+          : DateTime.now().min,
+      'date_to': timeRange != null
+          ? timeRange.end.millisecondsSinceEpoch
+          : DateTime.now().max,
+      'bucket_by_time': bucketByTime ?? 1,
+      'time_unit': timeUnit?.value ?? TimeUnit.days.value,
+    }).then(
       (response) => response
           .map((data) => DataPoint.fromJson(Map<String, dynamic>.from(data)))
           .toList(),
